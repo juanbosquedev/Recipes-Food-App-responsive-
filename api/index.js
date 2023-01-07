a740fac834da0bc79fa819cd8ec095ae2185c71a
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-01-07 20:05:06 -0300

    finish

--- api/index.js
+++ api/index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORTS = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORTS, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
