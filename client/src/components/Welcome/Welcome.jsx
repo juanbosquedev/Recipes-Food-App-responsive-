b0962d8379ca492891fc78110d6f831945bca348
Juan Bosque <86373520+juanchos85@users.noreply.github.com>
2023-02-07 14:00:32 -0300

    Update Welcome.jsx

--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -5,15 +5,13 @@ import Style from "../Welcome/Welcome.module.css";
 export default function Welcome() {
   return (
     <div className={Style.Welcome}>
-      <div className={Style.header}>
       <label className={Style.lbl}>Recipes Food</label>
       <div className={Style.intro}>
-      <p> Get your best and healthiest meal options. You can search for your favorite recipes by ingredients or type of food, and sort the result of it. In addition, we encourage you to leave us yours to share with our community</p>
+        <p>Get or create a recipes wich more suit to you!</p>
       </div>
-      <Link className={Style.text} to="/home" >
+      <Link className={Style.text} to="/home">
         Begin your own experience
       </Link>
     </div>
-      </div>
   );
 }
