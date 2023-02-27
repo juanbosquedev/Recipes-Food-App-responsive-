bc4f7b543c83206fac9438f1e36c31ffe9fa56b2
Juan Bosque <86373520+juanchos85@users.noreply.github.com>
2023-02-27 13:31:22 -0300

    Update Paginate.jsx

--- client/src/components/Paginate/Paginate.jsx
+++ client/src/components/Paginate/Paginate.jsx
@@ -4,18 +4,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { get_recipe } from "../../Reduxx/Actions/actions";
 import Style from "./Paginate.module.css";
 
-export default function Paginate({ paginado, setPaginado }) {
+export default function Paginate({ setPaginado }) {
   const recipes = useSelector((state) => state.recipes);
   const dispatch = useDispatch();
 
   let quantity = 9;
-  let total = paginado.length;
+  const total = recipes.length;
 
   let paginate = Math.ceil(total / quantity);
   const [current, setCurrent] = useState(1);
   const changePage = (current) => {
     if (current > 0 && current < paginate + 1) {
-      setPaginado(paginado.slice((current - 1) * quantity, quantity * current));
+      setPaginado(recipes.slice((current - 1) * quantity, quantity * current));
       setCurrent(current);
     }
   };
@@ -27,7 +27,69 @@ export default function Paginate({ paginado, setPaginado }) {
     <div className={Style.body}>
       {/* <span className={Style.btn}> */}
         <button
-          // hidden={current === 1 && true}
+          // hidden={current === 1 && true}import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { get_recipe } from "../../Reduxx/Actions/actions";
+import Style from "./Paginate.module.css";
+
+export default function Paginate({ setPaginado }) {
+  const recipes = useSelector((state) => state.recipes);
+  const dispatch = useDispatch();
+
+  let quantity = 9;
+  const total = recipes.length;
+
+  let paginate = Math.ceil(total / quantity);
+  const [current, setCurrent] = useState(1);
+  const changePage = (current) => {
+    if (current > 0 && current < paginate + 1) {
+      setPaginado(recipes.slice((current - 1) * quantity, quantity * current));
+      setCurrent(current);
+    }
+  };
+  useEffect(() => {
+    changePage(1);
+    get_recipe();
+  }, [recipes]);
+  return (
+    <div className={Style.body}>
+      <button
+        id={Style.btn}
+        className={`button ${current === 1}`}
+        onClick={() => changePage(1)}
+      >
+        Begin
+      </button>
+
+      <button
+        id="btn"
+        hidden={current === 1 && true}
+        className={`button ${current === 1}`}
+        onClick={() => changePage(current - 1)}
+      >
+        -
+      </button>
+      <button>{current}</button>
+      <button
+        hidden={current === paginate && true}
+        className={`button ${current === total}`}
+        onClick={() => changePage(current + 1)}
+      >
+        +
+      </button>
+
+      <button
+        id={Style.btn}
+        className={`button ${current === total}`}
+        onClick={() => changePage(paginate)}
+      >
+        End
+      </button>
+    </div>
+  );
+}
+
           id={Style.btn}
           className={`button ${current === 1}`}
           onClick={() => changePage(1)}
