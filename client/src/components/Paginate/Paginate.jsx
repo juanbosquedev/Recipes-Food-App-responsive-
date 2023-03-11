fb539efa7be2521978522cff90e1396e8909ed83
Juan Bosque <86373520+juanchos85@users.noreply.github.com>
2023-03-11 06:29:53 -0300

    Update Paginate.jsx

--- client/src/components/Paginate/Paginate.jsx
+++ client/src/components/Paginate/Paginate.jsx
@@ -25,35 +25,6 @@ export default function Paginate({ setPaginado }) {
   }, [recipes]);
   return (
     <div className={Style.body}>
-      {/* <span className={Style.btn}> */}
-        <button
-          // hidden={current === 1 && true}import React, { useState } from "react";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { get_recipe } from "../../Reduxx/Actions/actions";
-import Style from "./Paginate.module.css";
-
-export default function Paginate({ setPaginado }) {
-  const recipes = useSelector((state) => state.recipes);
-  const dispatch = useDispatch();
-
-  let quantity = 9;
-  const total = recipes.length;
-
-  let paginate = Math.ceil(total / quantity);
-  const [current, setCurrent] = useState(1);
-  const changePage = (current) => {
-    if (current > 0 && current < paginate + 1) {
-      setPaginado(recipes.slice((current - 1) * quantity, quantity * current));
-      setCurrent(current);
-    }
-  };
-  useEffect(() => {
-    changePage(1);
-    get_recipe();
-  }, [recipes]);
-  return (
-    <div className={Style.body}>
       <button
         id={Style.btn}
         className={`button ${current === 1}`}
@@ -86,44 +57,6 @@ export default function Paginate({ setPaginado }) {
       >
         End
       </button>
-    </div>
-  );
-}
-
-          id={Style.btn}
-          className={`button ${current === 1}`}
-          onClick={() => changePage(1)}
-        >
-          Begin
-        </button>
-      {/* </span> */}
-      <button
-        id="btn"
-        hidden={current === 1 && true}
-        className={`button ${current === 1}`}
-        onClick={() => changePage(current - 1)}
-      >
-        -
-      </button>
-      <button>{current}</button>
-      <button
-        hidden={current === paginate && true}
-        className={`button ${current === total}`}
-        onClick={() => changePage(current + 1)}
-      >
-        +
-      </button>
-      {/* <span className={Style.btn}> */}
-        <button
-          // hidden={current === paginate && true}
-          id={Style.btn}
-
-          className={`button ${current === total}`}
-          onClick={() => changePage(paginate)}
-        >
-          End
-        </button>
-      {/* </span> */}
     </div>
   );
 }
