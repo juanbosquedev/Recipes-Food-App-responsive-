bd7fa5e7f18519db93be8e2581d7757786a060f9
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-10-24 11:57:58 -0300

    update

--- api/src/app.js
+++ api/src/app.js
@@ -10,17 +10,6 @@ require("./db.js");
 const server = express();
 
 server.name = "API";
-// server.use(cors({
-//   origin: 'https://recipesjb.vercel.app', // Cambia esto con el origen de tu cliente
-//   credentials: true,
-//   methods: 'GET, POST, OPTIONS, PUT, DELETE',
-//   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-// }));
-
-
-
-
-
 
 server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
@@ -28,7 +17,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://recipesjb.vercel.app/home"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
