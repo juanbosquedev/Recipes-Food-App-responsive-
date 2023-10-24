90f548ccd2a704b5f55b77be832e3e35fed14b97
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-10-24 11:45:46 -0300

    CORS

--- api/src/app.js
+++ api/src/app.js
@@ -10,22 +10,33 @@ require("./db.js");
 const server = express();
 
 server.name = "API";
+server.use(cors({
+  origin: 'https://recipesjb.vercel.app', // Cambia esto con el origen de tu cliente
+  credentials: true,
+  methods: 'GET, POST, OPTIONS, PUT, DELETE',
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+}));
 
-server.use(cors());
+
+
+
+
+
+// server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 server.use("/", routes);
 
