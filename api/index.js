ad0897e5abfb8b6eae074e9463ef2702d212acf8
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-12-18 20:45:47 -0300

    api

--- api/index.js
+++ api/index.js
@@ -18,35 +18,32 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
+// Añadimos un middleware para manejar la conexión a la base de datos antes de las rutas
 server.use(async (req, res, next) => {
   try {
     const dbConnection = await conn();
-    req.dbConnection = dbConnection;
+    req.dbConnection = dbConnection; // Añadimos la conexión a la solicitud para que esté disponible en las rutas
     next();
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
 server.use("/", routes);
 
+// Resto del código...
 
 const { PORT } = process.env;
 
-conn()
-  .then((dbConnection) => {
-    server.listen(PORT, () => {
-      console.log(`Server is listening `);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database:", error);
+conn().then((dbConnection) => {
+  server.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
   });
+}).catch((error) => {
+  console.error('Error connecting to the database:', error);
+});
