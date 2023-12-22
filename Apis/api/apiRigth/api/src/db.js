05c7d5729a3be65b0856a8b8dbc197d44ec3e5b8
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-12-21 21:09:29 -0300

    native

--- Apis/api/apiRigth/api/src/db.js
+++ Apis/api/apiRigth/api/src/db.js
@@ -3,20 +3,20 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+const sequelize = new Sequelize(
+  'postgres://zgzngkwa:cqQXmFFZIk6FPx4l5B_mLlIbbHNpZOj9@flora.db.elephantsql.com/zgzngkwa',
   
-//   {
-//     logging: false,
-//     native: false,
-//   }
-// );
-const sequelize = new Sequelize('postgres://zgzngkwa:cqQXmFFZIk6FPx4l5B_mLlIbbHNpZOj9@flora.db.elephantsql.com/zgzngkwa', {
-  dialect: 'postgres',
-  host: 'flora.db.elephantsql.com',
+  {
+    logging: false,
+    native: false,
+  }
+);
+// const sequelize = new Sequelize('postgres://zgzngkwa:cqQXmFFZIk6FPx4l5B_mLlIbbHNpZOj9@flora.db.elephantsql.com/zgzngkwa', {
+//   dialect: 'postgres',
+//   host: 'flora.db.elephantsql.com',
  
-  logging: true,
-});
+//   logging: true,
+// });
 
 const getDatabaseConnection = async () => {
   try {
