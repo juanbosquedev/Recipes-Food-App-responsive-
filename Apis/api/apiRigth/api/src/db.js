7433b3e6252a3f9284746eb186fa4a0634109961
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-12-24 09:00:29 -0300

    db pool

--- Apis/api/apiRigth/api/src/db.js
+++ Apis/api/apiRigth/api/src/db.js
@@ -11,6 +11,12 @@ const sequelize = new Sequelize(`${POSTGRESQL_URL}`, {
     ssl: true,
   },
   logging: true,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 const getDatabaseConnection = async () => {
