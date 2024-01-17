de1fe99f0c642b1b9c79f9fccaa33aeecaf7a67e
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-01-17 20:21:11 -0300

    cleaning code

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,11 +3,8 @@ const { Router } = require("express");
 const { Op, Association } = require("sequelize");
 const { Sequelize } = require("sequelize");
 const { API_KEY } = process.env;
-const {
-  allDiets,
-} = require("../downloadData/downloading");
+const { allDiets } = require("../downloadData/downloading");
 const { Recipe, DietsTypes } = require("../db");
-
 
 const controllers = require("../controllers");
 
@@ -23,11 +20,10 @@ router.get("/recipesCreated", async (req, res) => {
     },
   });
   try {
-    if (!created) return res.status(400).send("recipe not created");
-    if (!created) return res.status(400).send("recipe not created");
+    if (!created) return res.status(400).send("there is not recipe created");
     return res.status(200).json(created);
   } catch (error) {
-    return res.status(400).json(error);
+    return res.status(405).json(error);
   }
 });
 
@@ -36,11 +32,13 @@ router.get("/recips/:name", controllers.getByName);
 router.post("/recipes", async (req, res) => {
   const { name, summary, steps, image, diets } = req.body;
 
-  if (!name || !summary || !steps || !diets)
+  if (!name || !summary || !steps || !diets) {
     return res.status(404).send("Required field uncompleted");
-  if (!image)
+  }
+  if (!image) {
     req.body.image =
       "https://media.istockphoto.com/id/1388791611/photo/teppanyaki-style.jpg?b=1&s=170667a&w=0&k=20&c=o4FL_2iyEO2XQiliXts-JphIFAhXg5BYlxvjxmBbh7E=";
+  }
   diets.map(async (el) => {
     const findDiet = await DietsTypes.findOrCreate({
       where: {
