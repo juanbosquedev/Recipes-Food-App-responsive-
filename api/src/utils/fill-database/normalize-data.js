7d5326c4c84077cf7ff0f5587b4736382cac29c3
juan <juan@shaper.us>
2024-10-11 13:06:30 -0300

    reafactor: new normalize function for data extern

---
+++ api/src/utils/fill-database/normalize-data.js
@@ -0,0 +1,44 @@
+const { sequelize } = require("../../database/connection.js");
+const { Recipe, DietsTypes } = sequelize.models;
+
+const storeRecipes = async (recipesFromApi) => {
+  try {
+    for (const recipeData of recipesFromApi) {
+    
+      const [recipe, created] = await Recipe.findOrCreate({
+        where: { idOriginal: recipeData.id },
+        defaults: {
+          idOriginal: recipeData.id,
+          name: recipeData.title,
+          summary: recipeData.summary,
+          healthScore: recipeData.healthScore,
+          dishTypes: recipeData.dishTypes.map((el) => el),
+          steps:
+            recipeData.analyzedInstructions &&
+            recipeData.analyzedInstructions.length > 0
+              ? recipeData.analyzedInstructions[0].steps.map((el) => el.step)
+              : [],
+          image: recipeData.image,
+          servings: recipeData.servings,
+          cookingTime: recipeData.readyInMinutes,
+        },
+      });
+
+      if (recipeData.diets && recipeData.diets.length > 0) {
+        const dietPromises = recipeData.diets.map(async (dietName) => {
+          const [diet] = await DietsTypes.findOrCreate({
+            where: { name: dietName },
+          });
+
+          await recipe.addDietsTypes(diet);
+        });
+
+        await Promise.all(dietPromises);
+      }
+    }
+  } catch (error) {
+    console.error(`Error processing recipe:`, error);
+  }
+};
+
+module.exports = { storeRecipes };
