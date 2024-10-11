e5f1b7b6ddeb0ddd63ba8c99d48583144b747915
juan <juan@shaper.us>
2024-10-11 14:41:39 -0300

    refactorized: recipe model

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,57 +1,54 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const Recipe = sequelize.define("Recipe", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      unique: true,
-      allowNull: false,
-      // type: DataTypes.UUID,
-      // defaultValue: DataTypes.UUIDV4,
-      // primaryKey: true,
-    },
-    idOriginal:{
-      type: DataTypes.INTEGER,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    summary: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    healthScore: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        min: 0,
-        max: 100
-      }
-    }, 
-    // dishTypes:{
-    //   type: DataTypes.ARRAY(DataTypes.STRING),
-    // },
-    // steps: {
-    //   type: DataTypes.ARRAY(DataTypes.STRING),
-    // },
-    image:{
-      type: DataTypes.STRING,
-    },
+  const Recipe = sequelize.define(
+    "Recipe",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+        allowNull: false,
+        // type: DataTypes.UUID,
+        // defaultValue: DataTypes.UUIDV4,
+        // primaryKey: true,
+      },
+      idOriginal: {
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      summary: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      healthScore: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 0,
+          max: 100,
+        },
+      },
 
-    servings:{
-      type: DataTypes.INTEGER,
-      allowNull:false,
-    },
-    cookingTime:{
-      type: DataTypes.INTEGER,
-      allowNull:false,
-    }
-  },{timestamps: false});
+      image: {
+        type: DataTypes.STRING,
+      },
 
+      servings: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      cookingTime: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
 
   return Recipe;
 };
-
