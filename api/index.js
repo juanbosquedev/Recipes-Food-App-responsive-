98bcc7edb8a5f284cb08d1416c3bfe78c43ac0e3
juan <juan@shaper.us>
2024-10-11 12:50:31 -0300

    Refactor index: Update db connection & sync logic

--- api/index.js
+++ api/index.js
@@ -1,47 +1,29 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const routes = require("./src/routes/index");
-const cors = require("cors");
+const { sequelize } = require("./src/database/connection");
+require("dotenv").config();
 
-const { conn } = require("./src/db.js");
-
-const server = express();
-server.name = "API";
-
-server.use(cors());
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
-server.use(cookieParser());
-server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-
-
-server.use(async (req, res, next) => {
-  try {
-    const dbConnection = await conn();
-    req.dbConnection = dbConnection; 
-    next();
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-server.use("/", routes);
-
+const server = require("./src/app");
 const { PORT } = process.env;
 
-conn().then((dbConnection) => {
-  server.listen(PORT, () => {
-    console.log(`Server is listening at http://localhost:${PORT}`);
-  });
-}).catch((error) => {
-  console.error('Error connecting to the database:', error);
-});
+require("./src/models/association/associations");
+
+const { fetchAndStoreApiData } = require('./src/utils/fill-database/populateDatabase');
+
+async function initializeServer() {
+  try {
+    
+    await sequelize.sync({ force: false });
+    await fetchAndStoreApiData();
+
+    console.log("Connection to the database established successfully.");
+
+
+    server.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error(`Unable to connect to the database: ${error.message}`);
+  }
+}
+
+initializeServer();
