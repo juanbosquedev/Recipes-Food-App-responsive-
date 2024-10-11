eb5d928fc9a51f5ac6c3c147a27b4290b09b347f
juan <juan@shaper.us>
2024-10-11 14:43:47 -0300

    feat: association models for recipe-diet

---
+++ api/src/models/association/relation-associations/Recipe-Diet.js
@@ -0,0 +1,6 @@
+const { sequelize } = require("../../../database/connection");
+
+const { Recipe, DietsTypes, RecipeDiet } = sequelize.models;
+
+Recipe.belongsToMany(DietsTypes, { through: RecipeDiet  });
+DietsTypes.belongsToMany(Recipe, { through: RecipeDiet  });
