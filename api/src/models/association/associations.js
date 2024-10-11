573a783743a11e052ef85ae4b8e0bf518b07fa04
juan <juan@shaper.us>
2024-10-11 14:42:39 -0300

    feat: index for association models

--- api/src/models/association/associations.js
+++
@@ -1,6 +0,0 @@
-const { sequelize } = require("../../database/connection");
-
-const { Recipe, DietsTypes, RecipeDiet } = sequelize.models;
-
-Recipe.belongsToMany(DietsTypes, { through: RecipeDiet  });
-DietsTypes.belongsToMany(Recipe, { through: RecipeDiet  });
