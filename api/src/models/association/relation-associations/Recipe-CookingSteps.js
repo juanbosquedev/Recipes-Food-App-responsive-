f2a5a5f015bb916b09bd58fe9429c4c54b27cd05
juan <juan@shaper.us>
2024-10-11 14:43:14 -0300

    feat: association models for recipe-steps

---
+++ api/src/models/association/relation-associations/Recipe-CookingSteps.js
@@ -0,0 +1,9 @@
+const { sequelize } = require("../../../database/connection");
+
+const { Recipe, CookingStep } = sequelize.models;
+
+
+
+ Recipe.hasMany(CookingStep);
+
+CookingStep.belongsTo( Recipe);
