37a470de15f99d055d5db3acb8c66e9bcb4101c7
juan <juan@shaper.us>
2024-10-15 10:40:48 -0300

    feat: get all diet by string(name or diet) param

--- api/src/controllers/getByName.js
+++ api/src/controllers/getByName.js
@@ -1,41 +1,50 @@
-const { Recipe, DietsTypes } = require("../db");
-const Sequelize = require("sequelize");
-const { Op, Association } = require("sequelize");
+const { Recipe, DietsTypes } = require("../database/connection");
+const { Op } = require("sequelize");
 
 module.exports = async (req, res) => {
-  const name = req.params.name;
+  const { name } = req.params;
+
   try {
-    if (!name) return res.status(401).send("Not a valid name");
-    if (name) {
-      const dieta = await DietsTypes.findOne({
-        where: {
-          name: name,
+    if (!name) {
+      return res.status(400).json({ error: "Invalid request: name is required" });
+    }
+
+    // Try to find a diet by the given name
+    const diet = await DietsTypes.findOne({
+      where: { name: { [Op.iLike]: `%${name}%` } },
+    });
+
+    // If the diet is found, fetch all recipes associated with this diet
+    if (diet) {
+      const recipesByDiet = await Recipe.findAll({
+        include: {
+          model: DietsTypes,
+          where: { id: diet.id }, // Filter recipes that are associated with this diet
         },
       });
-      if (dieta) {
-        let recipes = await Recipe.findAll();
-        let byDiets = recipes.filter((el) => el.diets.includes(dieta.name));
-        recipes.map(async (el) => {
-          await el.addDietsTypes(dieta.id);
-        });
-        if (byDiets) {
-          return res.status(200).json(byDiets);
-        }
-      } else if (name) {
-        const recipe = await Recipe.findAll({
-          where: {
-            name: { [Op.iLike]: `%${name}%` },
-          },
-        });
-        if (recipe.length > 0) {
-          return res.status(200).json(recipe);
-        } else {
-          return res.status(300).send("Try with another product!");
-        }
+
+      if (recipesByDiet.length > 0) {
+        return res.status(200).json(recipesByDiet);
+      } else {
+        return res.status(404).json({ message: "No recipes found for this diet" });
       }
     }
+
+    // If no diet is found, search for recipes by the given name
+    const recipesByName = await Recipe.findAll({
+      where: {
+        name: { [Op.iLike]: `%${name}%` }, // Search recipes with partial matching
+      },
+      include: DietsTypes, // Optionally include diet information for these recipes
+    });
+
+    if (recipesByName.length > 0) {
+      return res.status(200).json(recipesByName);
+    } else {
+      return res.status(404).json({ message: "No recipes found with that name" });
+    }
   } catch (error) {
-    console.log(error);
-    return res.status(405).json(error);
+    console.error("Error fetching recipes:", error);
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
