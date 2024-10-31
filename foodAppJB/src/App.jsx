7f3954f2601a36d8d8dbd80bd25107718429fad6
juan <juan@shaper.us>
2024-10-31 07:43:39 -0300

    feat: prevent show navbar while welcome component

--- foodAppJB/src/App.jsx
+++ foodAppJB/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 import Nav from "./components/Nav/Nav.jsx";
 import Home from "./components/Home/Home.jsx";
 import CardDetail from "./components/CardDetail/CardDetail.jsx";
@@ -9,18 +9,19 @@ import RecipesCreated from "./components/RecipesCreated/RecipesCreated";
 import CreateRecipe from "./components/CreateRecipe/CreateRecipe.jsx";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div>
+      {location.pathname !== "/" && <Nav />}
+      
       <Routes>
         <Route exact path="/" element={<Welcome />} />
-      </Routes>
-      <Nav />
-      <Routes>
         <Route exact path="/home" element={<Home />} />
         <Route path="/CardDetail/:id" element={<CardDetail />} />
         <Route path="/RecipesCreated" element={<RecipesCreated />} />
         <Route path="/CreateRecipe" element={<CreateRecipe />} />
-        <Route path="/*" element={<Navigate to="/"></Navigate>}></Route>
+        <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
     </div>
   );
